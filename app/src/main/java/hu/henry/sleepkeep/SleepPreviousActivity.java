15601fd38a4aefa320e60114fe0b74d4dca6b797
henryh1570 <henryhu@cpp.edu>
2017-04-18 17:47:39 -0700

    setting up sleep entries

--- app/src/main/java/hu/henry/sleepkeep/SleepPreviousActivity.java
+++ app/src/main/java/hu/henry/sleepkeep/SleepPreviousActivity.java
@@ -1,15 +1,30 @@
 package hu.henry.sleepkeep;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SleepPreviousActivity extends AppCompatActivity {
 
     Button doneButton;
+    TextView timePassedText;
+    String previousTimeString;
+    long previousTimeValue;
+    CountDownTimer clock;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +32,70 @@ public class SleepPreviousActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sleep_previous);
 
         doneButton = (Button) findViewById(R.id.done);
+        timePassedText = (TextView) findViewById(R.id.timePassed);
 
+        // TODO: Load the locked date string here
+        previousTimeString = "04/18/2017 16:55:03";
+        previousTimeValue = new Date(previousTimeString).getTime();
+
+        clock = getClock();
+        clock.start();
+
+        // Dialog box for confirming the finished entry
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder adb;
+                final EditText editText = new EditText(SleepPreviousActivity.this);
+                adb = new AlertDialog.Builder(SleepPreviousActivity.this);
+                adb.setTitle("Confirmation");
+                adb
+                        .setMessage("Finish this sleep entry?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                clock.onFinish();
+
+                                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+                                String date = sdf.format(new Date());
+                                Toast.makeText(SleepPreviousActivity.this, date + " Done! " + message,
+                                        Toast.LENGTH_LONG).show();
+
+                                //TODO: Save completed Entry to file here
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialog = adb.create();
+                alertDialog.show();
+
             }
         });
+    }
+
+    // Create a countdowntimer to constantly update the textview on offset time slept
+    public CountDownTimer getClock() {
+        CountDownTimer newtimer = new CountDownTimer(1000000000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                String date = sdf.format(new Date());
+
+                long diff = new Date(date.toString()).getTime() - previousTimeValue;
+                long diffRemainingMins = ((diff / (1000 * 60)) % 60);
+                long diffHours = (diff / (1000 * 60 * 60));
+                message = diffHours + "h:" + diffRemainingMins + "m";
+                timePassedText.setText(message);
+            }
+
+            public void onFinish() {
+                cancel();
+            }
+        };
+        return newtimer;
     }
 
     public static Intent newIntent(Context packageContext) {
--- app/src/main/java/hu/henry/sleepkeep/SleepTodayActivity.java
+++ app/src/main/java/hu/henry/sleepkeep/SleepTodayActivity.java
@@ -3,74 +3,105 @@ package hu.henry.sleepkeep;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SleepTodayActivity extends AppCompatActivity {
 
-    boolean isLocked = false;
-    Spinner todayBedHourSpinner;
-    Button lockInButton;
+    private boolean isLocked;
+    private Button lockInButton;
+    private TextView todayTimeText;
+    private TextView todayDateText;
+    private CountDownTimer clock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_today);
 
-        // TODO: Load saved data here
-        todayBedHourSpinner = (Spinner) findViewById(R.id.sleepTodayBedHour);
         lockInButton = (Button) findViewById(R.id.lockIn);
+        todayTimeText = (TextView) findViewById(R.id.todayCurrentTime);
+        todayDateText = (TextView) findViewById(R.id.todayCurrentDate);
 
-        todayBedHourSpinner.setSelection(8);
+        // TODO: Load saved data here
+        isLocked = false;
+
+        // Start the clock if today's entry is not done
+        // Otherwise do not use the clock and disable the lock in button
+        if (isLocked == false) {
+            clock = getClock();
+            clock.start();
+        } else {
+            // TODO: Load time / date here
+            todayTimeText.setText("LOADED TIME");
+            todayDateText.setText("LOADED DATE");
+            lockInButton.setEnabled(false);
+        }
 
         lockInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String hour = todayBedHourSpinner.getSelectedItem().toString();
-                int currentHour = new Time(System.currentTimeMillis()).getHours();
-                // TODO: Cross check current day's hour with the set hour for accuracy.
-                if (currentHour > Integer.parseInt(hour)) {
-                    Toast.makeText(SleepTodayActivity.this, "Invalid Hour",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    AlertDialog.Builder adb;
-                    final EditText editText = new EditText(SleepTodayActivity.this);
-                    adb = new AlertDialog.Builder(SleepTodayActivity.this);
-                    adb.setTitle("Confirmation");
-                    adb
-                            .setMessage("Are you ready to lock in now?")
-                            .setCancelable(false)
-                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    isLocked = true;
-                                    lockInButton.setEnabled(false);
+                AlertDialog.Builder adb;
+                final EditText editText = new EditText(SleepTodayActivity.this);
+                adb = new AlertDialog.Builder(SleepTodayActivity.this);
+                adb.setTitle("Confirmation");
+                adb
+                        .setMessage("Are you ready to lock in now?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                isLocked = true;
+                                lockInButton.setEnabled(false);
 
-                                    //TODO: Save data here
+                                //TODO: Save time and date data here
+                                todayTimeText.setTextColor(Color.RED);
+                                todayDateText.setTextColor(Color.RED);
+                                clock.onFinish();
 
-                                    Toast.makeText(SleepTodayActivity.this, "Locked In!",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    dialog.cancel();
-                                }
-                            });
+                                Toast.makeText(SleepTodayActivity.this, "Locked In!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
 
-                    AlertDialog alertDialog = adb.create();
-                    alertDialog.show();
-                }
+                AlertDialog alertDialog = adb.create();
+                alertDialog.show();
             }
         });
+    }
 
+    // Create a countdowntimer to constantly update the textview like a clock
+    public CountDownTimer getClock() {
+        CountDownTimer newtimer = new CountDownTimer(1000000000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                SimpleDateFormat sdftime = new SimpleDateFormat("HH:mm:ss");
+                SimpleDateFormat sdfdate = new SimpleDateFormat("MM/dd/yyyy");
+                String time = sdftime.format(new Date());
+                String date = sdfdate.format(new Date());
+
+                todayTimeText.setText(time);
+                todayDateText.setText(date);
+            }
+            public void onFinish() {
+                cancel();
+            }
+        };
+        return newtimer;
     }
 
     public static Intent newIntent(Context packageContext) {
